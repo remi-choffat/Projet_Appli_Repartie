eaf058d26964b5ad7fc477451230601226541fa7
vmaxime <mister.max0303@gmail.com>
2025-06-10 14:50:15 +0200

    interface serveur

---
+++ java/Nancy-Map/src/main/java/sae/http/ServiceServeurHttp.java
@@ -0,0 +1,17 @@
+package sae.http;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import sae.proxyHttp.ServiceProxy;
+
+/**
+ * ServiceServeurHttp
+ */
+public interface ServiceServeurHttp extends Remote {
+
+	public void enregisterService(ServiceProxy service) throws RemoteException;
+
+
+	
+}
