3705d0b2b321f6974e8648009c1f1cdfeddbd1ab
Cesareuh <mathieu.graff498@gmail.com>
2025-06-11 13:55:03 +0200

    service bd

--- java/Nancy-Map/src/main/java/sae/bd/ServiceBd.java
+++ java/Nancy-Map/src/main/java/sae/bd/ServiceBd.java
@@ -2,9 +2,10 @@ package sae.bd;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-
-import org.json.JSONObject;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 
 public interface ServiceBd extends Remote{
 	String getRestos() throws RemoteException;
+	String getTablesLibres(int idResto, Timestamp heure) throws RemoteException, SQLException;
 }
