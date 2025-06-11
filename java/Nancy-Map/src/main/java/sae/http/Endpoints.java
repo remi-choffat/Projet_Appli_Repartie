423d2124531c403a1300d20b0aaf9c39be47a944
vmaxime <mister.max0303@gmail.com>
2025-06-11 11:09:56 +0200

    Ajout Endpoints BD

---
+++ java/Nancy-Map/src/main/java/sae/http/Endpoints.java
@@ -0,0 +1,125 @@
+package sae.http;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import sae.bd.ServiceBd;
+import sae.proxyHttp.ServiceProxy;
+
+import static java.util.Map.entry;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Endpoints
+ */
+public class Endpoints {
+
+
+	private static Map<String, String> proxy_endpoints = Map.ofEntries(
+			entry("/incidents", "https://carto.g-ny.org/data/cifs/cifs_waze_v2.json")
+		);
+
+	private static enum DB_ENDPOINTS {
+		DB_RESTOS("/restos")
+			;
+
+		private final String endpoint; 
+		DB_ENDPOINTS(final String c) {this.endpoint = c;}
+		@Override
+		public String toString() {
+			return endpoint;
+		}
+
+
+	} 
+	private static void sendJson(HttpExchange exchange, JSONObject json) throws IOException {
+		OutputStream os = exchange.getResponseBody();
+		if (json ==null) {
+			sendError(exchange);
+		}
+
+		byte[] response = json.toString().getBytes();
+		exchange.getResponseHeaders().set("Content-Type", "application/json");
+		exchange.sendResponseHeaders(200, response.length);
+		os.write(response);
+		os.close();
+
+
+	}
+
+	private static void sendError(HttpExchange exchange) throws IOException {
+		exchange.sendResponseHeaders(400, -1);
+
+	}
+
+
+	public static void endpoints(HttpServer server) {
+		server.createContext("/", new HttpHandler() {
+			@Override
+			public void handle(HttpExchange exchange) throws IOException {
+				try {
+					OutputStream os = exchange.getResponseBody();
+
+					JSONObject json = new JSONObject();
+					JSONArray endpoints = new JSONArray();
+
+					proxy_endpoints.keySet().forEach(endpoints::put);
+					for (DB_ENDPOINTS values : DB_ENDPOINTS.values()) {
+						endpoints.put(values.toString());
+					}
+
+					json.put("endpoints", endpoints);
+
+					sendJson(exchange, json);
+				} catch (Exception e) {
+					e.printStackTrace();
+					sendError(exchange);
+				}
+			}
+		});
+
+	}
+
+	public static void proxy(HttpServer server, ServiceProxy proxy) {
+		for (Entry<String, String> e : proxy_endpoints.entrySet()) {
+			server.createContext(e.getKey(), new HttpHandler() {
+				@Override
+				public void handle(HttpExchange exchange) throws IOException {
+					label :  {
+						if (proxy==null) {
+							sendError(exchange);
+							break label;
+						}
+						JSONObject json = proxy.getJson(e.getValue());
+						sendJson(exchange, json);
+					}
+				}
+			});
+		}
+	}
+
+	public static void db(HttpServer server, ServiceBd bd) {
+		server.createContext(DB_ENDPOINTS.DB_RESTOS.toString(), new HttpHandler() {
+			@Override
+			public void handle(HttpExchange exchange) throws IOException {
+				label : {
+					if (bd == null) {
+						sendError(exchange);
+						break label;
+					}
+					JSONObject json = bd.getRestos();
+					sendJson(exchange, json);
+				}
+			}
+		});
+	}
+}
--- java/Nancy-Map/src/main/java/sae/http/Serveur.java
+++ java/Nancy-Map/src/main/java/sae/http/Serveur.java
@@ -8,15 +8,6 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map.Entry;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import sae.bd.ServiceBd;
@@ -28,11 +19,8 @@ import sae.proxyHttp.ServiceProxy;
 public class Serveur implements ServiceServeurHttp {
 
 
-
-	HashMap<String, String> proxy_endpoints = new HashMap<>();
-	HashMap<String, String> db_endpoints = new HashMap<>();
-
 	ServiceProxy proxy;
+	ServiceBd bd;
 
 
 	private HttpServer server;
@@ -42,71 +30,12 @@ public class Serveur implements ServiceServeurHttp {
 		server = HttpServer.create(inet, 0);
 
 	
-
-		proxy_endpoints.put("/incidents", "https://carto.g-ny.org/data/cifs/cifs_waze_v2.json");
-
-		db_endpoints.put("", "");
-
-
-		server.createContext("/", new HttpHandler() {
-			@Override
-			public void handle(HttpExchange exchange) throws IOException {
-				try {
-					OutputStream os = exchange.getResponseBody();
-
-					JSONObject json = new JSONObject();
-					JSONArray endpoints = new JSONArray();
-
-					proxy_endpoints.keySet().forEach(endpoints::put);
-					db_endpoints.keySet().forEach(endpoints::put);
-
-					json.put("endpoints", endpoints);
-
-					exchange.getResponseHeaders().set("Content-Type", "application/json");
-					byte[] res = json.toString().getBytes();
-					exchange.sendResponseHeaders(200, res.length);
-
-					os.write(res);
-					os.close();
-
-				} catch (Exception e) {
-					e.printStackTrace();
-					exchange.sendResponseHeaders(500, -1);
-				}
-								
-			}
-		});
+		Endpoints.endpoints(server);
+		Endpoints.proxy(server, proxy);
+		Endpoints.db(server, bd);
 
 
-
-		for (Entry<String, String> e : proxy_endpoints.entrySet()) {
-			server.createContext(e.getKey(), new HttpHandler() {
-				@Override
-				public void handle(HttpExchange exchange) throws IOException {
-					OutputStream os = exchange.getResponseBody();
-
-					label :  {
-						if (proxy==null) {
-							exchange.sendResponseHeaders(400, -1);
-							break label;
-						}
-						JSONObject json = proxy.getJson(e.getValue());
-
-						if (json ==null) {
-							exchange.sendResponseHeaders(400, -1);
-							break label;
-						}
-
-
-						byte[] response = json.toString().getBytes();
-						exchange.sendResponseHeaders(200, response.length);
-						os.write(response);
-					}
-					os.close();
-				}
-			});
 		}
-	}
 
 
 
@@ -127,7 +56,7 @@ public class Serveur implements ServiceServeurHttp {
 
 	@Override
 	public void enregisterServiceBd(ServiceBd service) throws RemoteException {
-		return;
+		this.bd = service;
 	}
 	
 }
