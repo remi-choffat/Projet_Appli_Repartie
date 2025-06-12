b71e5ac9f31a67f002b9390c51be3d72356f86b8
vmaxime <mister.max0303@gmail.com>
2025-06-12 11:32:26 +0200

    run scirpt'

--- java/Nancy-Map/runServer.sh
+++ java/Nancy-Map/runServer.sh
@@ -9,10 +9,13 @@
 # java sae/http/LancerServeur localhost 2000
 #
 
-
+mvn clean compile assembly:single
 killall rmiregistry 
 sleep 0.5
 rmiregistry $1 -J-classpath -Jtarget/Nancy-Map-1-jar-with-dependencies.jar & 
 sleep 0.5
-mvn clean compile assembly:single
-java -cp target/Nancy-Map-1-jar-with-dependencies.jar sae.http.LancerServeur localhost $1 9090
+java -cp target/Nancy-Map-1-jar-with-dependencies.jar sae.http.LancerServeur localhost $1 9090 & 
+sleep 0.5
+java -cp target/Nancy-Map-1-jar-with-dependencies.jar sae.bd.LancerService localhost $1 localhost $1 &
+sleep 0.5
+java -cp target/Nancy-Map-1-jar-with-dependencies.jar sae.proxyHttp.LancerService localhost $1 localhost $1 & 
