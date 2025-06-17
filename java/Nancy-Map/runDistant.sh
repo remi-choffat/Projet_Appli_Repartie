bd1bf24365a06884f013b6b9a16d3a725a9adde9
vmaxime <mister.max0303@gmail.com>
2025-06-17 08:40:04 +0200

    fix script runDistant.sh

--- java/Nancy-Map/runDistant.sh
+++ java/Nancy-Map/runDistant.sh
@@ -33,32 +33,31 @@ if [ -n "$6" ]; then hostservicedb=$6 ; fi
 if [ -n "$7" ]; then portservicedb=$7 ; fi
 if [ -n "$8" ]; then portapi=$8 ; fi
 
-mvn clean compile assembly:single 
+# mvn clean compile assembly:single 
+# check_succes $! "mvn"
 
-check_succes $! "compile"
+dir=$PWD
 
-ssh $user@$hostserveur "mkdir nancyapprepo/"
-check_succes $! "mvn"
 
-ssh $user@$hostserveur "cd nancyapprepo && rmiregistry $portserver -J-classpath -Jtarget/Nancy-Map-1-jar-with-dependencies.jar > registry1.logs 2>&1"
+ssh $user@$hostserveur "cd $dir && rmiregistry $portserver -J-classpath -Jtarget/Nancy-Map-1-jar-with-dependencies.jar > registry1.logs 2>&1"
 check_succes $! "rmiregistry server"
 
 
 if [ "$hostserveur" != "$hostserviceproxy" ] && [ "$portserver" != "$portserviceproxy" ] ; then 
-	ssh $user@$hostserviceproxy "cd nancyapprepo && rmiregistry $portserviceproxy -J-classpath -Jtarget/Nancy-Map-1-jar-with-dependencies.jar > registry2.logs 2>&1"
+	ssh $user@$hostserviceproxy "cd $dir && rmiregistry $portserviceproxy -J-classpath -Jtarget/Nancy-Map-1-jar-with-dependencies.jar > registry2.logs 2>&1"
 check_succes $! "rmiregistry service proxyHttp"
 fi
 
 if [ "$hostserveur" != "$hostservicedb" ] && [ "$portserver" != "$portservicedb" ] ; then 
-	ssh $user@$hostservicedb "cd nancyapprepo && rmiregistry $portservicedb -J-classpath -Jtarget/Nancy-Map-1-jar-with-dependencies.jar > registry3.logs 2>&1"
+	ssh $user@$hostservicedb "cd $dir && rmiregistry $portservicedb -J-classpath -Jtarget/Nancy-Map-1-jar-with-dependencies.jar > registry3.logs 2>&1"
 check_succes $! "rmiregistry server servicebd"
 fi
 
 
 
-ssh $user@$hostserveur "cd nancyapprepo && java -cp target/Nancy-Map-1-jar-with-dependencies.jar sae.http.LancerServeur localhost $portserver $portapi > server.logs 2>&1 &"
+ssh $user@$hostserveur "cd $dir && java -cp target/Nancy-Map-1-jar-with-dependencies.jar sae.http.LancerServeur localhost $portserver $portapi > server.logs 2>&1 &"
 check_succes $! "server"
-ssh $user@$hostserviceproxy "cd nancyapprepo && java -cp target/Nancy-Map-1-jar-with-dependencies.jar sae.bd.LancerService localhost $portserviceproxy $hostserveur $portserver $ > servicebd.logs 2>&1 &"
+ssh $user@$hostserviceproxy "cd $dir && java -cp target/Nancy-Map-1-jar-with-dependencies.jar sae.bd.LancerService localhost $portserviceproxy $hostserveur $portserver $ > servicebd.logs 2>&1 &"
 check_succes $! "serviceproxy"
-ssh $user@$hostservicedb "cd nancyapprepo && java -cp target/Nancy-Map-1-jar-with-dependencies.jar sae.proxyHttp.LancerService localhost $portservicedb $hostserveur $portserver > serviceproxy.logs 2>&1  &"
+ssh $user@$hostservicedb "cd $dir && java -cp target/Nancy-Map-1-jar-with-dependencies.jar sae.proxyHttp.LancerService localhost $portservicedb $hostserveur $portserver > serviceproxy.logs 2>&1  &"
 check_succes $! "servicebd"
